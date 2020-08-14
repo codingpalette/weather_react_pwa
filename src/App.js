9af91d1b0226339f1f9b306ac951776b3eee8b33
codingpalette <codingpalette@gmail.com>
2020-08-14 22:28:16 +0900

    weather card create

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 const App = () => {
     const [query, setQuery] = useState('Seoul');
+    const [weather, setWeather] = useState({});
 
     const search = async (e) => {
         if(e.key === 'Enter') {
             const data = await fetchWeather(query);
             console.log(data);
-
+            setWeather(data);
+            setQuery('');
         }
     }
 
@@ -25,6 +27,22 @@ const App = () => {
                     onChange={(e) => setQuery(e.target.value)}
                     onKeyPress={search}
                 />
+                {weather.main && (
+                    <div className="city">
+                        <h2 className="city-name">
+                            <span>{weather.name}</span>
+                            <sup>{weather.sys.country}</sup>
+                        </h2>
+                        <div className="city-temp">
+                            {Math.round(weather.main.temp)}
+                            <sup>&deg;C </sup>
+                        </div>
+                        <div className="info">
+                            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} alt=""/>
+                            <p>{weather.weather[0].description}</p>
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     )
